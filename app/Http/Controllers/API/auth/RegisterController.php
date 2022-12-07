93c54ff343e3752643ee5bc3ebd11a7002573f8c
Angga Arifandi <anggadwiarifandi96@gmail.com>
2022-12-08 01:05:38 +0700

    register new

--- app/Http/Controllers/API/auth/RegisterController.php
+++ app/Http/Controllers/API/auth/RegisterController.php
@@ -84,6 +84,8 @@ class RegisterController extends BaseController
     {
         $credentials = request(['email', 'password']);
 
+        dd($credentials);
+
         if (! $token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
