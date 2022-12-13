f541c0005f3f6cc2e5b41e11861550380c135351
imamsatya <imamsatyawedhatama@gmail.com>
2022-12-13 13:46:58 +0700

    add permohonan crud

--- app/Http/Controllers/Frontend/LayananPPID/DataPermohonanController.php
+++ app/Http/Controllers/Frontend/LayananPPID/DataPermohonanController.php
@@ -180,7 +180,7 @@ class DataPermohonanController extends Controller
     public function ppidDataPermohonanSpec(Request $request, $id)
     {
         $result = DB::table('ppid_permohonan')
-            ->select('ppid_permohonan.*', 'ppid_mendapatkan.name as cara_mendapatkan', 'ppid_memberikan.name as cara_memberikan')
+            ->select('ppid_permohonan.*', 'ppid_mendapatkan.name as cara_mendapatkan', 'ppid_memberikan.name as cara_memberikan', 'status_permohonan.id_status as id_status_permohonan', 'status.name as nama_status_permohonan')
             ->join('ppid_mendapatkan', 'ppid_mendapatkan.id', '=', 'ppid_permohonan.id_mendapatkan')
             ->join('ppid_memberikan', 'ppid_memberikan.id', '=', 'ppid_permohonan.id_cara')
             ->join('status_permohonan', 'status_permohonan.id_ppid_permohonan', '=', 'ppid_permohonan.id')
